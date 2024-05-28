78c555fcdf08471840081451e873ccd94d756085
Paul <paul@theapplegates.com>
2024-05-27 23:52:31 -0400

    Corrected export syntax for ESM in readingtime filter

--- src/_config/filters/readingtime.js
+++ src/_config/filters/readingtime.js
@@ -1,30 +1,31 @@
 // calculate the time to read of a chunk of text (to the nearest minute)
 // based on https://www.bobmonsour.com/posts/calculating-reading-time/
-const nunjucks = require('nunjucks');
+import nunjucks from 'nunjucks';
 nunjucks.configure({ autoescape: true });
 
-export default = function(text) {
-    var content = new String(text);
+export default function readingTime(text) {
+    const content = String(text);
     const speed = 240; // reading speed in words per minute
-  
+
     // remove all html elements
-    var re = /(&lt;.*?&gt;)|(<[^>]+>)/gi;
-    var plain = content.replace(re, "");
-  
+    const re = /(<.*?>)|(<[^>]+>)/gi;
+    let plain = content.replace(re, "");
+
     // replace all newlines and 's with spaces
-    var plain = plain.replace(/\s+|'s/g, " ");
-  
+    plain = plain.replace(/\s+|'s/g, " ");
+
     // create array of all the words in the post & count them
-    var words = plain.split(" ");
-    var count = words.length;
-  
+    const words = plain.split(" ");
+    const count = words.length;
+
     // calculate the reading time
-    var readingTime = Math.round(count / speed);
+    const readingTime = Math.round(count / speed);
     if (readingTime === 0) {
-    	return this.ctx.translations[this.page.lang || this.ctx.lang].readingTime.underMinute;
+        return this.ctx.translations[this.page.lang || this.ctx.lang].readingTime.underMinute;
     } else if (readingTime === 1) {
-    	return this.ctx.translations[this.page.lang || this.ctx.lang].readingTime.minute;
+        return this.ctx.translations[this.page.lang || this.ctx.lang].readingTime.minute;
     } else {
-    	return nunjucks.renderString(this.ctx.translations[this.page.lang || this.ctx.lang].readingTime.other, { minutes: readingTime});
+        return nunjucks.renderString(this.ctx.translations[this.page.lang || this.ctx.lang].readingTime.other, { minutes: readingTime});
     }
-};
+}
+
