1398cb863871fb020589814a6d2d9ee55d98cc38
Paul <paul@theapplegates.com>
2024-05-27 23:51:02 -0400

    Corrected export syntax for ESM in dates filter

--- src/_config/filters/dates.js
+++ src/_config/filters/dates.js
@@ -1,8 +1,9 @@
 const formatDate = function (date, format = { weekday: 'long', year: 'numeric', month: 'long', day: 'numeric' }) {
     const d = new Date(date);
     return d.toLocaleDateString(this.ctx.locales[this.page.lang || this.ctx.lang].locale, format);
-}
+};
 
-export default = {
+export default {
     formatDate
 };
+
