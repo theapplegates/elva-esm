7c1c3ac03b37f146b68ff71e0ffdd0d82ab104b5
Paul <paul@theapplegates.com>
2024-05-27 23:51:52 -0400

    Corrected export syntax for ESM in random filter

--- src/_config/filters/random.js
+++ src/_config/filters/random.js
@@ -1,9 +1,10 @@
 // return a random item from a collection, excluding the current page
 // {% set collection = collections.posts | random(page) %}
-export default = function(collections, avoid) {
+export default function random(collections, avoid) {
     let selected = collections[Math.floor(Math.random() * collections.length)];
     while (selected.url === avoid.url) {
         selected = collections[Math.floor(Math.random() * collections.length)];
     }
     return [selected];
 };
+
