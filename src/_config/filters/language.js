3da85bf3d31d73650ecbcd632166cfb8899f42a9
Paul <paul@theapplegates.com>
2024-05-27 23:51:18 -0400

    Corrected export syntax for ESM in language filter

--- src/_config/filters/language.js
+++ src/_config/filters/language.js
@@ -1,5 +1,6 @@
-export default = function languageFilter(collection, lang) {
+export default function languageFilter(collection, lang) {
     if (!lang) lang = this.page.lang || this.ctx.lang;
     const filtered = collection.filter(item => item.page.lang == lang);
     return filtered;
 }
+
