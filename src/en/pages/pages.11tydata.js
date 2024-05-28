ace40aa5fe6784608b951cdfeeb970252b5968e6
Paul <paul@theapplegates.com>
2024-05-28 10:25:23 -0400

    Update pages.11tydata.js

--- src/en/pages/pages.11tydata.js
+++ src/en/pages/pages.11tydata.js
@@ -1,11 +1,13 @@
+import slugify from './utils/slugify'; // Or define slugify directly in this file
+
 export default {
-    layout: 'page',
-    tags: 'page',
-    permalink: function(data) {
-        if (data.seo?.slug) {
-            return `/${data.lang}/${this.slugify(data.seo.slug)}/`;
-        } else {
-            return `/${data.lang}/${this.slugify(data.page.fileSlug)}/`;
-        }
+  layout: 'page',
+  tags: 'page',
+  permalink: (data) => {
+    if (data.seo?.slug) {
+      return `/${data.lang}/${slugify(data.seo.slug)}/`;
+    } else {
+      return `/${data.lang}/${slugify(data.page.fileSlug)}/`;
     }
+  }
 };
