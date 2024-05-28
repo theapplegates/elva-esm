72b0c2fa61a3adb410f4291b88317328cb152f4a
Paul <paul@theapplegates.com>
2024-05-27 23:49:26 -0400

    Corrected export syntax for ESM in base64 filter

--- src/_config/filters/base64.js
+++ src/_config/filters/base64.js
@@ -1,3 +1,4 @@
-export default = function base64(text) {
-    return Buffer.from(text).toString('base64')
+export default function base64(text) {
+    return Buffer.from(text).toString('base64');
 };
+
